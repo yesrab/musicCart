be77c4a5d1eb00f7a5b9dc5c00e858f499026287
yesrab <yesrab.ali111@gmail.com>
2024-03-21 23:35:42 +0530

    implemeted basic image server

--- src/server/server.js
+++ src/server/server.js
@@ -3,4 +3,13 @@ require("dotenv").config();
 require("express-async-errors");
 const PORT = 8080;
 const app = express();
+const fs = require("fs");
+const path = require("path");
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+app.get("/image", (req, res) => {
+  const pathName = path.resolve(__dirname, "./images/image1.png");
+  res.sendFile(pathName);
+});
 app.listen(PORT, () => console.log(`Server is listening port ${PORT}...`));
